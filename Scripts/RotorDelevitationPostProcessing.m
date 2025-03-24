b859be0c421dbd7efd8df33d73a9239038052e3a
jjansev <jjansev@mathworks.com>
2025-03-24 11:43:16 +0100

    Added AMB rotor Delevitation Example

---
+++ Scripts/RotorDelevitationPostProcessing.m
@@ -0,0 +1,4 @@
+% Rotor Delevitation Post processing
+
+comet([out.recordout1{1}.Values.Data' -1.5 -1.5 1.5 1.5]',[out.recordout1{2}.Values.Data' -1.5 1.5 1.5 -1.5]')
+comet([out.recordout1{1}.Values.Data' -1.5 -1.5 1.5 1.5]',[out.recordout1{2}.Values.Data' -1.5 1.5 1.5 -1.5]')
